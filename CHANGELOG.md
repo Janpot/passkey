6939315999124d6c75b8ef79f7e0f75753804b9e
Jan Potoms <potoms.jan@gmail.com>
2016-01-17 13:07:22 +0100

    Add extend method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 3.0.0
+
+  - `ttl` now returns current ttl.
+  - added `extend` method
+
 ## 2.0.0
 
   - Replace `then-redis` with `redis` package
--- test/passkey.spec.js
+++ test/passkey.spec.js
@@ -85,21 +85,6 @@ describe('passkey', function () {
 
   });
 
-  it('should error when calling ttl with no arguments', function () {
-
-    return keychain.lock('test', 1000)
-      .then(function (lock) {
-        return lock.ttl();
-      })
-      .then(function () {
-        assert(false, 'Expected to fail');
-      }, function (err) {
-        assert.notInstanceOf(err, passkey.LockError);
-        assert.strictEqual(err.message, '.ttl must be called with a number');
-      });
-
-  });
-
   it('should error when calling .lock with no arguments', function () {
 
     return keychain.lock()
@@ -110,6 +95,81 @@ describe('passkey', function () {
         assert.strictEqual(err.message, '.lock must be called with a string');
       });
 
+  });
+  
+  it('should extend ttl', function () {
+    var lock;
+    return keychain.lock('test', 10)
+      .then(function (_lock) {
+        lock = _lock;
+        return lock.ttl(100);
+      })
+      .then(function (ttl) {
+        assert.strictEqual(ttl, 100);
+        return Promise.delay(20);
+      })
+      .then(function () {
+        return lock.ttl();
+      })
+      .then(function (ttl) {
+        assert(ttl > 0);
+      });
+  });
+  
+  it('ttl should unlock when called with 0', function () {
+    return keychain.lock('test', 1000)
+      .then(function (lock) {
+        return lock.ttl(0);
+      })
+      .then(function (ttl) {
+        assert.strictEqual(ttl, 0);
+        return keychain.lock('test', 1000);
+      });
+  });
+
+  
+  it('should extend the lock to last ttl', function () {
+    var lock;
+    return keychain.lock('test', 10000)
+      .then(function (_lock) {
+        lock = _lock;
+        return Promise.fromCallback(function (cb) {
+          // simulate passage of time
+          client.pexpire('test', 2000, cb);
+        });
+      })
+      .then(function () {
+        return lock.ttl();
+      })
+      .then(function (ttl) {
+        assert(ttl <= 2000, 'forced expire should have worked');
+        return lock.extend();
+      })
+      .then(function (ttl) {
+        assert(ttl > 9000);
+        return lock.ttl();
+      })
+      .then(function (ttl) {
+        assert(ttl > 9000);
+        return lock.ttl(20000);
+      })
+      .then(function (_lock) {
+        return Promise.fromCallback(function (cb) {
+          // simulate passage of time
+          client.pexpire('test', 2000, cb);
+        });
+      })
+      .then(function (ttl) {
+        assert(ttl <= 2000, 'forced expire should have worked');
+        return lock.extend();
+      })
+      .then(function (ttl) {
+        assert(ttl > 19000);
+        return lock.ttl();
+      })
+      .then(function (ttl) {
+        assert(ttl > 19000);
+      });
   });
 
 });
